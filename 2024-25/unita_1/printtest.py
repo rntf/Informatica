386f323fbdc1d280665a94c1f90f2dcdda46e278
Renato <32295975+rntf@users.noreply.github.com>
2024-10-02 12:05:00 +0200

    Add files via upload

---
+++ 2024-25/unita_1/printtest.py
@@ -0,0 +1,19 @@
+##
+#  Programma che illustra il comportamento della funzione print.
+#
+
+# Visualizza 7
+print(3 + 4) 
+      
+# Visualizza "Hello World!" su due righe.
+print("Hello") 
+print("World!")
+
+# Visualizza più valori usando una singola invocazione di print.
+print("My favorite numbers are", 3 + 4, "and", 3 + 10) 
+
+# Visualizza tre righe di testo, la seconda delle quali vuota.
+print("Goodbye")   
+print()
+print("Hope to see you again")
+
