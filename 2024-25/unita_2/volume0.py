327268b6fc8e2ca4b9684dbe911207960b32c32a
Renato <32295975+rntf@users.noreply.github.com>
2024-10-03 10:11:46 +0200

    Create volume0.py

---
+++ 2024-25/unita_2/volume0.py
@@ -0,0 +1,14 @@
+# Costanti
+CAPACITA_BOTTIGLIA = 0.75
+CAPACITA_LATTINA = 0.33
+
+# Dati in input
+num_bottiglie = 4
+num_lattine = 6
+
+# Calcoli
+totale = 0
+totale = num_bottiglie * CAPACITA_BOTTIGLIA
+totale = totale + num_lattine * CAPACITA_LATTINA
+
+print(totale)
